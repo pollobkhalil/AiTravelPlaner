1fe55a5a1922cd498ab68c586bd00a5e5922266d
Your Name <you@example.com>
2024-10-04 13:09:10 +0600

    sign-in and sign-up push

--- app/auth/sign-in/index.js
+++ app/auth/sign-in/index.js
@@ -3,8 +3,28 @@ import React from 'react'
 
 export default function SingIn() {
   return (
-    <View>
-      <Text>SingIn</Text>
+    <View style={{
+      padding:25,
+      marginTop:60,
+      backgroundColor:'white',
+      height:'100%',
+    }}>
+      <Text style={{
+        fontFamily:'bold',
+        fontSize: 30,
+        fontFamily: 'Outfit-Bold',
+        paddingBottom:20
+
+      }}>Let's Sign You In </Text>
+      <Text style={{
+        
+        fontSize: 20,
+        fontFamily:'Outfit-Regular'
+      }}>Welcome Back </Text>
+      <Text style={{
+        fontSize: 30,
+        
+      }}> You've been missed </Text>
     </View>
   )
 }
